a85af7405169903dd02b7a4b99355693336b2af6
Paul P <gerin_paul@hotmail.com>
2020-03-24 17:14:27 -0400

    -added rocket script

---
+++ Assets/RocketController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    new Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        //thrusters
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(new Vector3(0,8.5f,0));
+        }
+
+        //rotate left
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+        //rotate right
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+
+    }
+}
