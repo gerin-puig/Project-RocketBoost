57eb16123c47d256476c4457ebc16ebd4b28b131
Paul P <gerin_paul@hotmail.com>
2020-03-26 17:55:36 -0400

    -added scene loading -added playerdeath

--- Assets/RocketController.cs
+++ Assets/RocketController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RocketController : MonoBehaviour
 {
@@ -10,6 +11,15 @@ public class RocketController : MonoBehaviour
 
     new Rigidbody rigidbody;
     AudioSource audioSource;
+
+    enum State
+    {
+        Alive,
+        Dying,
+        Transcending
+    }
+
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -21,21 +31,44 @@ public class RocketController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RotateShip();
-        Thruster();
+        if (state == State.Alive)
+        {
+            RotateShip();
+            Thruster();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(state != State.Alive)
+            return;
+
         string colName = collision.gameObject.tag;
-        if(colName == "Friendly")
+        if(colName == "Finish")
         {
-            
+            state = State.Transcending;
+            Invoke("LoadNextScene", 2f);
         }
-        else
+        else if(colName == "Friendly")
         {
 
         }
+        else
+        {
+            state = State.Dying;
+            Invoke("PlayerDeath", 2f);
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void PlayerDeath()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        state = State.Alive;
     }
 
     private void RotateShip()
